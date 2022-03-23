bcffcdc49b6c2505a854c3f09ae3825f42e0002f
RouhallahAfsharpour <abadanastro@gmail.com>
2022-03-23 10:23:39 +0100

    error for WebSecurityConfiguration

---
+++ src/main/java/com/example/Student_Swagger/WebSecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.Student_Swagger;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**");
+    }
+}
